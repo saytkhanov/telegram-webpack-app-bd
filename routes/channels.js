1f3d3c2d44074a167261a17e3185aaf163b13d95
Mohmad <magomedov951@mail.ru>
2021-06-09 20:06:51 +0300

    ispravili 4

--- routes/channels.js
+++ routes/channels.js
@@ -3,9 +3,10 @@ const channelsController = require("../controllers/channels")
 const router = Router()
 
 //router.get("/random")
+router.get("/channels", channelsController.getAllChannels)
 router.get("/channels/:id", channelsController.getChannels)
 router.get("/channels/:id/category", channelsController.getChannelsByCategory)
 
-router.post("/channels", channelsController.createChannels)
+router.post("/channels/:id", channelsController.createChannels)
 
 module.exports = router
